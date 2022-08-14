484183f8209e354848077a50ce3ab3b9fd311ea2
Alex Schwartz <ajs67@buffalo.edu>
2022-08-13 23:07:47 -0400

    Added profile picture uploading and saving profile settings to database

--- core/views.py
+++ core/views.py
@@ -15,6 +15,30 @@ def index(request):
 @login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST":
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profile_img
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profile_img = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profile_img = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+
     return render(request, 'setting.html', {'user_profile': user_profile})
 
 
