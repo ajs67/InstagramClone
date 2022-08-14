97f83ccb74196b54865e3876a6de736e61ef0877
Alex Schwartz <ajs67@buffalo.edu>
2022-08-14 17:28:03 -0400

    Added post likes

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import  login_required
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 
 # Create your views here.
@@ -29,6 +29,28 @@ def upload(request):
         new_post.save()
         return redirect('/')
     else:
+        return redirect('/')
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.num_of_likes = post.num_of_likes + 1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.num_of_likes -= 1
+        post.save()
         return redirect('/')
 
 
