f641e62a11e1885d892f05ed417857f42429deea
Alex Schwartz <ajs67@buffalo.edu>
2022-08-16 17:50:25 -0400

    Added conditional follow button text

--- core/views.py
+++ core/views.py
@@ -61,11 +61,20 @@ def profile(request, pk):
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
 
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
         'user_post_length': user_post_length,
+        'button_text': button_text,
 
     }
 
