de8173bb39f7139632b70c7b1fd415b1eca7056b
Alex Schwartz <ajs67@buffalo.edu>
2022-08-19 00:20:38 -0400

    Added search function

--- core/models.py
+++ core/models.py
@@ -37,6 +37,7 @@ class LikePost(models.Model):
     def __str__(self):
         return self.username
 
+
 class FollowersCount(models.Model):
     follower = models.CharField(max_length=100)
     user = models.CharField(max_length=100)
