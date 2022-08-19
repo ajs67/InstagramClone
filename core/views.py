203b4b3bc7fc99254111753464ff4ca1b79008bb
Alex Schwartz <ajs67@buffalo.edu>
2022-08-19 13:28:51 -0400

    Added user suggestions

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Post, LikePost, FollowersCount
 from itertools import chain
+import random as r
 
 # Create your views here.
 
@@ -28,8 +29,35 @@ def index(request):
 
     feed_list = list(chain(*feed))
 
+    # users you can follow suggestions
+
+    all_users = User.objects.all()
+    user_following_all = []
+
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+    current_user = User.objects.filter(username=request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    r.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+
+    for ids in username_profile:
+        profile_list = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_list)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
+
     posts = Post.objects.all()
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list})
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
 
 
 @login_required(login_url='signin')
